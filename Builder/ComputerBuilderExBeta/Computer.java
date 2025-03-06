f563707ad60a2d6c55fc3577e7def7d7a87046e0
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-06 13:12:40 +0200

    Create Computer.java

---
+++ Builder/ComputerBuilderExBeta/Computer.java
@@ -0,0 +1,24 @@
+public class Computer {
+    private String cpu;
+    private String ram;
+    private String storage;
+
+
+public void setCpu(String cpu) {
+    this.cpu = cpu;
+}
+
+public void setRam(String ram){
+    this.ram = ram;
+}
+
+public void setStorage(String storage) {
+    this.storage = storage;
+}
+
+@Override
+public String toString(){
+    return "Computer [CPU: " + cpu + ", RAM:" + ram + ", Storage:" + storage + "]";
+
+}
+}
