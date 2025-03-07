0e7a6e1db433be755b251b764aec85c9494fdadc
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-07 15:36:06 +0200

    Create ComputerBuilder.java

---
+++ Builder/Exercitiu2Computer/ComputerBuilder.java
@@ -0,0 +1,15 @@
+//INTERFACE
+public interface ComputerBuilder {
+    
+    void buildComputerType();
+
+    void buildCpu();
+
+    void buildRam();
+
+    void buildStorage();
+
+    void buildManufacturingType();
+
+    Computer getComputer();
+}
