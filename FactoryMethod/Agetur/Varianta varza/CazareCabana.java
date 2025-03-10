456a85c9b1bc2986a676b9a2bb4f807d251d8313
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-10 15:15:26 +0200

    Create CazareCabana.java

---
+++ FactoryMethod/Agetur/Varianta varza/CazareCabana.java
@@ -0,0 +1,7 @@
+public class CazareCabana implements PachetTuristic {
+    @Override 
+    public String descriere(){
+        return "Pachet de cazare la cabana cu lemne de foc incluse";
+    }
+    
+}
