0f769c2b94148015b9320c6c9737d4e0a5d47a3c
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-10 15:18:45 +0200

    Create PachetTuristic.java

---
+++ FactoryMethod/Agetur/Varianta varza/PachetTuristic.java
@@ -0,0 +1,6 @@
+//Interfata!! : contract pentru toate tipurile de pachete
+//Are metoda descriere - returneaza detalii despre pachetul turistic
+// si atunci toate pachetele concrete trebuie sa furnizeze propria implementare pt aceasta metoda
+public interface PachetTuristic {
+    String descriere();
+}
