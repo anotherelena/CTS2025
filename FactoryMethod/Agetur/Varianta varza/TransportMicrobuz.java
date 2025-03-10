2122771992dca8b9abaa50885d54ab1c7ea68452
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-10 15:19:25 +0200

    Create TransportMicrobuz.java

---
+++ FactoryMethod/Agetur/Varianta varza/TransportMicrobuz.java
@@ -0,0 +1,8 @@
+public class TransportMicrobuz implements PachetTuristic {
+    @Override
+
+    public String descriere(){
+        return "Pachet transport cu microbuzul, bilet dus-intors, fara loc pre-asignat.";
+    }
+    
+}
