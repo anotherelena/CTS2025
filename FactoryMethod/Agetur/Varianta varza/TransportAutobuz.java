1f833d4502486b581694a1d8f2d657a164caddc5
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-10 15:19:02 +0200

    Create TransportAutobuz.java

---
+++ FactoryMethod/Agetur/Varianta varza/TransportAutobuz.java
@@ -0,0 +1,6 @@
+public class TransportAutobuz implements PachetTuristic{
+    @Override
+    public String descriere(){
+        return "Pachet de transport cu autobuzul, bilet dus-intors.";
+    }
+}
