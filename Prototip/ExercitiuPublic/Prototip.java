84e20b2fe5d0ed6f6c392c6f01292c5c544a16bb
Elena Chiriac <96721647+anotherelena@users.noreply.github.com>
2025-03-13 12:25:17 +0200

    Create Prototip.java

---
+++ Prototip/ExercitiuPublic/Prototip.java
@@ -0,0 +1,4 @@
+//declaratia de interfata prototip cu metoda prototip care va folosi interfata clone de la supraclasa object practic si va fi oevrriden pt deep copy
+interface Prototip {
+    public Prototip cloneaza();
+}
